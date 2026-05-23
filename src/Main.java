70109e0b4641fb75db1aebd593e369ca14518a32
Esteban-Dubois <esteban.dubois@edu.univ-fcomte.fr>
2026-05-23 14:35:31 +0000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 import boardifier.control.StageFactory;
 import boardifier.model.Model;
 import tablut.view.View;
@@ -5,11 +6,65 @@ import tablut.control.Controller;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("=== LANCEMENT DU TABLUT (VERSION TEXTE) ===");
+        System.out.println("Tablut Launch - Text version");
 
+        Scanner inputScanner = new Scanner(System.in);
         Model model = new Model();
-        model.addHumanPlayer("Jarod (Noirs)");
-        model.addHumanPlayer("MonPote (Blancs)");
+
+        int gameModeSelection = 0;
+        while (gameModeSelection < 1 || gameModeSelection > 3) {
+            System.out.println("Select Game Mode:");
+            System.out.println("1 - Player vs Player");
+            System.out.println("2 - Player vs Artificial Intelligence 1");
+            System.out.println("3 - Player vs Artificial Intelligence 2");
+            System.out.print("Your choice (1-3): ");
+            
+            if (inputScanner.hasNextInt()) {
+                gameModeSelection = inputScanner.nextInt();
+            } else {
+                inputScanner.next();
+            }
+        }
+
+        int playerColorSelection = 0;
+        while (playerColorSelection < 1 || playerColorSelection > 2) {
+            System.out.println("Select your color:");
+            System.out.println("1 - Black (Attackers, play first)");
+            System.out.println("2 - White (Defenders)");
+            System.out.print("Your choice (1-2): ");
+            
+            if (inputScanner.hasNextInt()) {
+                playerColorSelection = inputScanner.nextInt();
+            } else {
+                inputScanner.next();
+            }
+        }
+
+        if (gameModeSelection == 1) {
+            if (playerColorSelection == 1) {
+                model.addHumanPlayer("Player 1 (Black)");
+                model.addHumanPlayer("Player 2 (White)");
+            } else {
+                model.addHumanPlayer("Player 2 (Black)");
+                model.addHumanPlayer("Player 1 (White)");
+            }
+        } else if (gameModeSelection == 2) {
+            if (playerColorSelection == 1) {
+                model.addHumanPlayer("Player (Black)");
+                model.addComputerPlayer("Artificial Intelligence 1 (White)");
+            } else {
+                model.addComputerPlayer("Artificial Intelligence 1 (Black)");
+                model.addHumanPlayer("Player (White)");
+            }
+        } else {
+            if (playerColorSelection == 1) {
+                model.addHumanPlayer("Player (Black)");
+                model.addComputerPlayer("Artificial Intelligence 2 (White)");
+            } else {
+                model.addComputerPlayer("Artificial Intelligence 2 (Black)");
+                model.addHumanPlayer("Player (White)");
+            }
+        }
 
         View view = new View(model);
         Controller controller = new Controller(model, view);
